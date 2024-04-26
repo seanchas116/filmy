7d0a5d757ce4757899fdc628b74dc82f1f6dcda6
Ryohei Ikegami <iofg2100@gmail.com>
2024-04-26 10:52:12 +0900

    Insert ellipse/text

--- packages/app/components/editor/compisition-view.tsx
+++ packages/app/components/editor/compisition-view.tsx
@@ -4,6 +4,7 @@ import { observer } from "mobx-react-lite";
 import { useRef } from "react";
 import { nanoid } from "nanoid";
 import { action } from "mobx";
+import { ShapeData } from "@/document/schema";
 
 export const CompositionView: React.FC = observer(() => {
   const editorState = useEditorState();
@@ -30,22 +31,41 @@ const EventTarget = observer(() => {
   } | null>(null);
 
   const onPointerDown = action((e: React.PointerEvent) => {
-    if (editorState.tool === "rectangle") {
+    if (editorState.tool) {
       const document = editorState.document;
 
       const rootNode = document.currentTimelineItem.node;
+
+      const shape: ShapeData =
+        editorState.tool === "rectangle"
+          ? {
+              type: "rectangle",
+              x: e.nativeEvent.offsetX,
+              y: e.nativeEvent.offsetY,
+              w: 100,
+              h: 100,
+            }
+          : editorState.tool === "ellipse"
+          ? {
+              type: "ellipse",
+              x: e.nativeEvent.offsetX,
+              y: e.nativeEvent.offsetY,
+              w: 50,
+              h: 50,
+            }
+          : {
+              type: "text",
+              x: e.nativeEvent.offsetX,
+              y: e.nativeEvent.offsetY,
+              text: "Text",
+            };
+
       const node = document.nodes.add(nanoid(), {
         parent: rootNode.id,
         order: rootNode.children.length,
         detail: {
           type: "shape",
-          shape: {
-            type: "rectangle",
-            x: e.nativeEvent.offsetX,
-            y: e.nativeEvent.offsetY,
-            w: 100,
-            h: 100,
-          },
+          shape,
           fill: {
             type: "solid",
             color: { r: 255, g: 0, b: 0, a: 1 },
@@ -61,7 +81,7 @@ const EventTarget = observer(() => {
       });
 
       insertionStateRef.current = {
-        type: "rectangle",
+        type: editorState.tool,
         node,
         startX: e.nativeEvent.offsetX,
         startY: e.nativeEvent.offsetY,
@@ -70,10 +90,10 @@ const EventTarget = observer(() => {
   });
 
   const onPointerMove = action((e: React.PointerEvent) => {
-    if (editorState.tool === "rectangle" && insertionStateRef.current) {
+    if (editorState.tool && insertionStateRef.current) {
       const state = insertionStateRef.current;
       const detail = state.node.data.detail;
-      if (detail && detail.shape.type === "rectangle") {
+      if (detail?.shape) {
         const x1 = state.startX;
         const y1 = state.startY;
         const x2 = e.nativeEvent.offsetX;
@@ -84,16 +104,21 @@ const EventTarget = observer(() => {
         const w = Math.abs(x2 - x1);
         const h = Math.abs(y2 - y1);
 
-        state.node.detail = {
-          ...detail,
-          shape: {
-            ...detail.shape,
-            x,
-            y,
-            w,
-            h,
-          },
-        };
+        if (
+          detail.shape.type === "rectangle" ||
+          detail.shape.type === "ellipse"
+        ) {
+          state.node.detail = {
+            ...detail,
+            shape: {
+              ...detail.shape,
+              x,
+              y,
+              w,
+              h,
+            },
+          };
+        }
       }
     }
   });
@@ -149,10 +174,10 @@ const ShapeRenderer: React.FC<{
       case "ellipse":
         return (
           <ellipse
-            cx={detail.shape.x}
-            cy={detail.shape.y}
-            rx={detail.shape.w}
-            ry={detail.shape.h}
+            cx={detail.shape.x + detail.shape.w / 2}
+            cy={detail.shape.y + detail.shape.h / 2}
+            rx={detail.shape.w / 2}
+            ry={detail.shape.h / 2}
             fill="red"
           />
         );
--- packages/app/components/editor/tool-bar.tsx
+++ packages/app/components/editor/tool-bar.tsx
@@ -2,30 +2,41 @@ import { Icon } from "@iconify/react";
 import { useEditorState } from "./use-editor-state";
 import { action } from "mobx";
 import { observer } from "mobx-react-lite";
+import tw from "tailwind-styled-components";
+
+const IconButton = tw.button`
+  p-2 hover:bg-gray-100 rounded-lg aria-pressed:bg-blue-100 aria-pressed:text-blue-500
+`;
 
 export const ToolBar: React.FC = observer(() => {
   const editorState = useEditorState();
 
   return (
     <div className="h-10 bg-white border-b border-gray-200 flex items-center px-2">
-      <button
-        className="p-2 hover:bg-gray-100 rounded-lg aria-pressed:bg-blue-100 aria-pressed:text-blue-500"
+      <IconButton
         aria-pressed={editorState.tool === "rectangle"}
         onClick={action(() => {
           editorState.tool = "rectangle";
         })}
       >
-        <Icon icon="material-symbols:check-box-outline-blank" />
-      </button>
-      <button
-        className="p-2 hover:bg-gray-100 rounded-lg aria-pressed:bg-blue-100 aria-pressed:text-blue-500"
+        <Icon icon="material-symbols:square-outline-rounded" />
+      </IconButton>
+      <IconButton
         aria-pressed={editorState.tool === "ellipse"}
         onClick={action(() => {
           editorState.tool = "ellipse";
         })}
       >
+        <Icon icon="material-symbols:circle-outline" />
+      </IconButton>
+      <IconButton
+        aria-pressed={editorState.tool === "text"}
+        onClick={action(() => {
+          editorState.tool = "text";
+        })}
+      >
         <Icon icon="material-symbols:title-rounded" />
-      </button>
+      </IconButton>
     </div>
   );
 });
