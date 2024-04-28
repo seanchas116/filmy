a5f2f7c7cda8b1bc7f9944a509b033b8038bb355
Ryohei Ikegami <iofg2100@gmail.com>
2024-04-28 18:50:55 +0900

    Make client only

---
+++ packages/app/app/client-only.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Editor } from "@/editor/views/editor";
+import { EditorState } from "@/editor/state/editor-state";
+import { EditorStateProvider } from "@/editor/views/use-editor-state";
+import { useState } from "react";
+
+export default function ClientOnly() {
+  const [editorState] = useState(() => new EditorState());
+
+  return (
+    <EditorStateProvider value={editorState}>
+      <Editor />
+    </EditorStateProvider>
+  );
+}
--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { Editor } from "@/editor/views/editor";
-import { EditorState } from "@/editor/state/editor-state";
-import { EditorStateProvider } from "@/editor/views/use-editor-state";
-import { useState } from "react";
+import dynamic from "next/dynamic";
+
+const ClientOnly = dynamic(() => import("./client-only"), {
+  ssr: false,
+});
 
 export default function Home() {
-  const [editorState] = useState(() => new EditorState());
-
-  return (
-    <EditorStateProvider value={editorState}>
-      <Editor />
-    </EditorStateProvider>
-  );
+  return <ClientOnly />;
 }
