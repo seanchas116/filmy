c235fd336a24b1d76a2bc1c789dbc90cbf0a6126
Ryohei Ikegami <iofg2100@gmail.com>
2024-04-28 18:13:47 +0900

    Fix position

---
+++ packages/app/editor/views/viewport/use-observe-dom-client-rect.ts
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { action } from "mobx";
+import { Rect } from "paintvec";
+import { useEditorState } from "../use-editor-state";
+
+export function useObserveDOMClientRect(ref: React.RefObject<HTMLElement>) {
+  const editorState = useEditorState();
+
+  useEffect(() => {
+    const elem = ref.current;
+    if (!elem) {
+      return;
+    }
+
+    const updateViewportClientRect = action(() => {
+      editorState.scroll.domClientRect = Rect.from(
+        elem.getBoundingClientRect()
+      );
+    });
+
+    updateViewportClientRect();
+
+    const resizeObserver = new ResizeObserver(updateViewportClientRect);
+    resizeObserver.observe(elem);
+
+    window.addEventListener("scroll", updateViewportClientRect);
+
+    return () => {
+      resizeObserver.disconnect();
+      window.removeEventListener("scroll", updateViewportClientRect);
+    };
+  }, []);
+}
--- packages/app/editor/views/viewport/viewport.tsx
+++ packages/app/editor/views/viewport/viewport.tsx
@@ -1,11 +1,18 @@
 import { CompositionView } from "./compisition-view";
 import { useEditorState } from "../use-editor-state";
 import { EventTarget } from "./event-target";
+import { useRef } from "react";
+import { useObserveDOMClientRect } from "./use-observe-dom-client-rect";
 
 export function Viewport() {
   const editorState = useEditorState();
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useObserveDOMClientRect(ref);
+
   return (
-    <div className="bg-gray-100 relative">
+    <div className="bg-gray-100 relative" ref={ref}>
       <div
         className="absolute"
         style={{
