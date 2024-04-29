fa2a275b587a7662975ea3a0e9517919ce1c1c62
Ryohei Ikegami <iofg2100@gmail.com>
2024-04-29 15:42:20 +0900

    Show timeline item

--- packages/app/editor/views/timeline/timeline-editor.tsx
+++ packages/app/editor/views/timeline/timeline-editor.tsx
@@ -6,6 +6,8 @@ export const TimelineEditor: React.FC = () => {
     ...editorState.document.timelines.instances.values(),
   ].toSorted((a, b) => a.data.order - b.data.order); // TODO: cache sorting
 
+  const scale = 0.1;
+
   return (
     <div className="h-64 bg-white border-t border-gray-200 grid grid-cols-[auto_1fr]">
       <div className="w-64 border-r border-gray-200">
@@ -18,10 +20,19 @@ export const TimelineEditor: React.FC = () => {
       <div>
         {timelines.map((timeline) => {
           return (
-            <div key={timeline.id} className="flex">
-              {timeline.items.map((item) => (
-                <div key={item.id}>{item.id}</div>
-              ))}
+            <div className="p-1" key={timeline.id}>
+              <div className="flex relative h-10">
+                {timeline.items.map((item) => (
+                  <div
+                    key={item.id}
+                    className="bg-gray-100 absolute h-full top-0 rounded-lg border border-gray-200"
+                    style={{
+                      left: item.data.start * scale,
+                      width: item.data.duration * scale,
+                    }}
+                  ></div>
+                ))}
+              </div>
             </div>
           );
         })}
