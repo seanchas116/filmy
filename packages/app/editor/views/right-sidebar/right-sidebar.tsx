92ba28d874dbda8c72d006986be982c6b1d428fd
Ryohei Ikegami <iofg2100@gmail.com>
2024-05-01 18:26:13 +0900

    Fill & stroke edit

--- packages/app/editor/views/right-sidebar/right-sidebar.tsx
+++ packages/app/editor/views/right-sidebar/right-sidebar.tsx
@@ -4,15 +4,19 @@ import { MIXED, sameOrMixed } from "@/utils/mixed";
 import { observer } from "mobx-react-lite";
 import tw from "tailwind-styled-components";
 import { action } from "mobx";
+import { twMerge } from "tailwind-merge";
+import { ReactNode } from "react";
+import { Icon } from "@iconify/react";
 
 const InputWrap: React.FC<{
-  label: string;
+  label: ReactNode;
   children: React.ReactNode;
-}> = ({ label, children }) => {
+  className?: string;
+}> = ({ label, children, className }) => {
   return (
-    <div className="relative h-8">
+    <div className={twMerge("relative h-8", className)}>
       {children}
-      <div className="pointer-events-none absolute inset-y-0 leading-8 left-2 text-[10px] text-gray-400">
+      <div className="pointer-events-none absolute inset-y-0 left-2 text-[10px] text-gray-400 flex items-center">
         {label}
       </div>
     </div>
@@ -31,9 +35,16 @@ export const RightSideBar: React.FC = observer(() => {
   const y = sameOrMixed(selectedNodes.map((node) => node.data.y));
   const width = sameOrMixed(selectedNodes.map((node) => node.data.w));
   const height = sameOrMixed(selectedNodes.map((node) => node.data.h));
+  const fill = sameOrMixed(selectedNodes.map((node) => node.data.fill));
+  const strokeFill = sameOrMixed(
+    selectedNodes.map((node) => node.data.stroke?.fill)
+  );
+  const strokeWidth = sameOrMixed(
+    selectedNodes.map((node) => node.data.stroke?.width)
+  );
 
   return (
-    <div className="w-[256px] bg-white border-l border-gray-200">
+    <div className="w-[256px] bg-white border-l border-gray-200 text-xs">
       <div className="p-2 flex flex-col gap-2">
         <div className="grid grid-cols-2 gap-2">
           <InputWrap label="X">
@@ -101,6 +112,79 @@ export const RightSideBar: React.FC = observer(() => {
                   node.data = {
                     ...node.data,
                     h: n,
+                  };
+                }
+              })}
+            />
+          </InputWrap>
+        </div>
+      </div>
+      <div className="p-2 flex flex-col gap-2">
+        <h3>Fill</h3>
+        <input
+          type="color"
+          className="rounded-lg px-1 h-8"
+          value={fill === MIXED ? "#000000" : fill?.hex ?? "#000000"}
+          onChange={action((e) => {
+            const value = e.target.value;
+            for (const node of selectedNodes) {
+              node.data = {
+                ...node.data,
+                fill: {
+                  type: "solid",
+                  hex: value,
+                },
+              };
+            }
+          })}
+        />
+      </div>
+      <div className="p-2 flex flex-col gap-2">
+        <h3>Stroke</h3>
+        <div className="flex gap-2">
+          <input
+            type="color"
+            className="rounded-lg px-1 h-8"
+            value={
+              strokeFill === MIXED ? "#000000" : strokeFill?.hex ?? "#000000"
+            }
+            onChange={action((e) => {
+              const value = e.target.value;
+              for (const node of selectedNodes) {
+                node.data = {
+                  ...node.data,
+                  stroke: {
+                    width: node.data.stroke?.width ?? 1,
+                    fill: {
+                      type: "solid",
+                      hex: value,
+                    },
+                  },
+                };
+              }
+            })}
+          />
+          <InputWrap
+            label={<Icon icon="material-symbols:line-weight-rounded" />}
+            className="w-1/3"
+          >
+            <InputBody
+              value={strokeWidth === MIXED ? "" : String(strokeWidth ?? "")}
+              onChange={action((value) => {
+                const n = Number(value);
+                if (isNaN(n)) {
+                  return;
+                }
+                for (const node of selectedNodes) {
+                  node.data = {
+                    ...node.data,
+                    stroke: {
+                      width: n,
+                      fill: node.data.stroke?.fill ?? {
+                        type: "solid",
+                        hex: "#000000",
+                      },
+                    },
                   };
                 }
               })}
