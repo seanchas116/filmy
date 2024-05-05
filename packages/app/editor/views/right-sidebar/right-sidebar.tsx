5c0bfe4927fc1d07da264ff80cee8ca9e5a48fea
Ryohei Ikegami <iofg2100@gmail.com>
2024-05-05 13:44:43 +0900

    Use Radix tabs

--- packages/app/editor/views/right-sidebar/right-sidebar.tsx
+++ packages/app/editor/views/right-sidebar/right-sidebar.tsx
@@ -3,33 +3,38 @@ import { NodePropertyEditor } from "./node-property-editor";
 import { AnimationPropertyEditor } from "./animation-property-editor";
 import { useEditorState } from "../use-editor-state";
 import { action } from "mobx";
+import * as Tabs from "@radix-ui/react-tabs";
 
 export const RightSideBar: React.FC = observer(() => {
   const editorState = useEditorState();
   return (
-    <div className="w-[256px] bg-white border-l border-gray-200 text-xs">
-      <div className="p-2 flex">
-        <button
-          className="p-1 aria-pressed:text-blue-500"
-          aria-pressed={editorState.mode == "design"}
-          onClick={action(() => {
-            editorState.mode = "design";
-          })}
+    <Tabs.Root
+      value={editorState.mode}
+      onValueChange={action((value) => {
+        editorState.mode = value as "design" | "animate";
+      })}
+      className="w-[256px] bg-white border-l border-gray-200 text-xs"
+    >
+      <Tabs.List className="p-2 flex">
+        <Tabs.Trigger
+          value="design"
+          className="p-1 aria-selected:text-blue-500"
         >
           Design
-        </button>
-        <button
-          className="p-1 aria-pressed:text-blue-500"
-          aria-pressed={editorState.mode == "animate"}
-          onClick={action(() => {
-            editorState.mode = "animate";
-          })}
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="animate"
+          className="p-1 aria-selected:text-blue-500"
         >
           Animate
-        </button>
-      </div>
-      <NodePropertyEditor />
-      <AnimationPropertyEditor />
-    </div>
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="design">
+        <NodePropertyEditor />
+      </Tabs.Content>
+      <Tabs.Content value="animate">
+        <AnimationPropertyEditor />
+      </Tabs.Content>
+    </Tabs.Root>
   );
 });
