4dc28b3ce0038678f1b584004b45506c96e89f4e
Ryohei Ikegami <iofg2100@gmail.com>
2024-05-05 15:15:07 +0900

    Add some animation add buttons

--- packages/app/editor/views/right-sidebar/animation-property-editor.tsx
+++ packages/app/editor/views/right-sidebar/animation-property-editor.tsx
@@ -3,12 +3,24 @@ import { useEditorState } from "../use-editor-state";
 import { NumberInput } from "./input";
 import { action } from "mobx";
 
+const AnimationAddView: React.FC = observer(() => {
+  return (
+    <div className="p-2">
+      <h2>Add Animation</h2>
+      <div className="grid grid-cols-2 gap-2">
+        <button className="bg-gray-200 p-2">Slide In</button>
+        <button className="bg-gray-200 p-2">Fade In</button>
+      </div>
+    </div>
+  );
+});
+
 export const AnimationPropertyEditor: React.FC = observer(() => {
   const editorState = useEditorState();
   const selectedAnimations = editorState.document.selection.animations;
 
   if (!selectedAnimations.length) {
-    return null;
+    return <AnimationAddView />;
   }
 
   const animation = selectedAnimations[0];
