5bd84c63e80f1260dbd58f4e54de2d4aee6fd757
Ryohei Ikegami <iofg2100@gmail.com>
2024-05-06 20:42:42 +0900

    Add keyframe buttons at the side of input buttons

--- packages/app/editor/views/right-sidebar/node-property-editor.tsx
+++ packages/app/editor/views/right-sidebar/node-property-editor.tsx
@@ -6,6 +6,14 @@ import { Icon } from "@iconify/react";
 import { flattenGroup } from "@/document/node";
 import { NumberInput } from "./input";
 
+const KeyframeButton = () => {
+  return (
+    <button className="text-base text-gray-400">
+      <Icon icon="material-symbols:stat-0-outline" />
+    </button>
+  );
+};
+
 export const NodePropertyEditor: React.FC = observer(() => {
   const editorState = useEditorState();
   const selectedNodes = flattenGroup(editorState.document.selection.nodes);
@@ -31,121 +39,144 @@ export const NodePropertyEditor: React.FC = observer(() => {
     <>
       <div className="p-2 flex flex-col gap-2">
         <div className="grid grid-cols-2 gap-2">
-          <NumberInput
-            label="X"
-            value={x}
-            onChangeValue={action((value) => {
-              for (const node of selectedNodes) {
-                node.data = { ...node.data, x: value };
-              }
-            })}
-          />
-          <NumberInput
-            label="Y"
-            value={y}
-            onChangeValue={action((value) => {
-              for (const node of selectedNodes) {
-                node.data = { ...node.data, y: value };
-              }
-            })}
-          />
+          <div className="grid grid-cols-[auto_1fr]">
+            <KeyframeButton />
+            <NumberInput
+              label="X"
+              value={x}
+              onChangeValue={action((value) => {
+                for (const node of selectedNodes) {
+                  node.data = { ...node.data, x: value };
+                }
+              })}
+            />
+          </div>
+          <div className="grid grid-cols-[auto_1fr]">
+            <KeyframeButton />
+            <NumberInput
+              label="Y"
+              value={y}
+              onChangeValue={action((value) => {
+                for (const node of selectedNodes) {
+                  node.data = { ...node.data, y: value };
+                }
+              })}
+            />
+          </div>
         </div>
         <div className="grid grid-cols-2 gap-2">
-          <NumberInput
-            label="W"
-            value={width}
-            onChangeValue={action((value) => {
-              for (const node of selectedNodes) {
-                node.data = { ...node.data, w: value };
-              }
-            })}
-          />
-          <NumberInput
-            label="H"
-            value={height}
-            onChangeValue={action((value) => {
-              for (const node of selectedNodes) {
-                node.data = { ...node.data, h: value };
-              }
-            })}
-          />
+          <div className="grid grid-cols-[auto_1fr]">
+            <KeyframeButton />
+            <NumberInput
+              label="W"
+              value={width}
+              onChangeValue={action((value) => {
+                for (const node of selectedNodes) {
+                  node.data = { ...node.data, w: value };
+                }
+              })}
+            />
+          </div>
+          <div className="grid grid-cols-[auto_1fr]">
+            <KeyframeButton />
+            <NumberInput
+              label="H"
+              value={height}
+              onChangeValue={action((value) => {
+                for (const node of selectedNodes) {
+                  node.data = { ...node.data, h: value };
+                }
+              })}
+            />
+          </div>
         </div>
         <div className="grid grid-cols-2 gap-2">
-          <NumberInput
-            label={<Icon icon="material-symbols:opacity" />}
-            value={opacity}
-            onChangeValue={action((value) => {
-              for (const node of selectedNodes) {
-                node.data = { ...node.data, opacity: value };
-              }
-            })}
-          />
+          <div className="grid grid-cols-[auto_1fr]">
+            <KeyframeButton />
+            <NumberInput
+              label={<Icon icon="material-symbols:opacity" />}
+              value={opacity}
+              onChangeValue={action((value) => {
+                for (const node of selectedNodes) {
+                  node.data = { ...node.data, opacity: value };
+                }
+              })}
+            />
+          </div>
         </div>
       </div>
       <div className="p-2 flex flex-col gap-2">
         <h3>Fill</h3>
-        <input
-          type="color"
-          className="rounded-lg px-1 h-8"
-          value={fill === MIXED ? "#000000" : fill?.hex ?? "#000000"}
-          onChange={action((e) => {
-            const value = e.target.value;
-            for (const node of selectedNodes) {
-              node.data = {
-                ...node.data,
-                fill: {
-                  type: "solid",
-                  hex: value,
-                },
-              };
-            }
-          })}
-        />
-      </div>
-      <div className="p-2 flex flex-col gap-2">
-        <h3>Stroke</h3>
-        <div className="flex gap-2">
+        <div className="flex">
+          <KeyframeButton />
           <input
             type="color"
             className="rounded-lg px-1 h-8"
-            value={
-              strokeFill === MIXED ? "#000000" : strokeFill?.hex ?? "#000000"
-            }
+            value={fill === MIXED ? "#000000" : fill?.hex ?? "#000000"}
             onChange={action((e) => {
               const value = e.target.value;
               for (const node of selectedNodes) {
                 node.data = {
                   ...node.data,
-                  stroke: {
-                    width: node.data.stroke?.width ?? 1,
-                    fill: {
-                      type: "solid",
-                      hex: value,
-                    },
+                  fill: {
+                    type: "solid",
+                    hex: value,
                   },
                 };
               }
             })}
           />
-          <NumberInput
-            label={<Icon icon="material-symbols:line-weight-rounded" />}
-            className="w-1/3"
-            value={strokeWidth}
-            onChangeValue={action((value) => {
-              for (const node of selectedNodes) {
-                node.data = {
-                  ...node.data,
-                  stroke: {
-                    width: value,
-                    fill: node.data.stroke?.fill ?? {
-                      type: "solid",
-                      hex: "#000000",
-                    },
-                  },
-                };
+        </div>
+      </div>
+      <div className="p-2 flex flex-col gap-2">
+        <h3>Stroke</h3>
+        <div className="flex gap-2">
+          <div className="flex">
+            <KeyframeButton />
+            <input
+              type="color"
+              className="rounded-lg px-1 h-8"
+              value={
+                strokeFill === MIXED ? "#000000" : strokeFill?.hex ?? "#000000"
               }
-            })}
-          />
+              onChange={action((e) => {
+                const value = e.target.value;
+                for (const node of selectedNodes) {
+                  node.data = {
+                    ...node.data,
+                    stroke: {
+                      width: node.data.stroke?.width ?? 1,
+                      fill: {
+                        type: "solid",
+                        hex: value,
+                      },
+                    },
+                  };
+                }
+              })}
+            />
+          </div>
+          <div className="grid grid-cols-[auto_1fr] w-1/2">
+            <KeyframeButton />
+            <NumberInput
+              label={<Icon icon="material-symbols:line-weight-rounded" />}
+              value={strokeWidth}
+              onChangeValue={action((value) => {
+                for (const node of selectedNodes) {
+                  node.data = {
+                    ...node.data,
+                    stroke: {
+                      width: value,
+                      fill: node.data.stroke?.fill ?? {
+                        type: "solid",
+                        hex: "#000000",
+                      },
+                    },
+                  };
+                }
+              })}
+            />
+          </div>
         </div>
       </div>
     </>
